4da94cf4da0deffe13cf19a436dfcc9b52f12d8f
JustACupOfTeh <tehsiangen@gmail.com>
2023-01-11 01:31:39 +0800

    Models for Lifestyle added.

---
+++ eCO2Tracker/Models/Lifestyle.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCO2Tracker.Models
+{
+    public class Lifestyle
+    {
+        public string entryID { get; set; } = generateUuid();
+        [Required, MaxLength(100)]
+        [Display(Name = "Title")]
+        public string entryTitle { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Description")]
+        public string entryDesc { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Impact")]
+        public string entryImpact { get; set; } = string.Empty;
+        [Required, MaxLength(150)]
+        [Display(Name = "Reference")]
+        public string entryReferences { get; set; } = string.Empty;
+        [Required, MaxLength(50)]
+        public string entryImageURL { get; set; } = string.Empty;
+        public DateTime entryDate { get; set; } = DateTime.Now;
+        public bool entryStatus { get; set; } = false;
+
+        public static string generateUuid()
+        {
+            Guid guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+    }
+}
