7c3ff5130e970405447bb8dc8430dade10c085c1
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-08 18:38:50 +0800

    Update User.cs

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace eCO2TrackerWebAPI.Models
+namespace eCO2Tracker.Models
 {
     public class User
     {
