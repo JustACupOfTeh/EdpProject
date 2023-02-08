a0a67d0c48a3f1a57fc94476709af0d11e01eccd
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-08 18:40:34 +0800

    Update 20230130155934_InitialCreate.cs

--- eCO2Tracker/Migrations/20230130155934_InitialCreate.cs
+++ eCO2Tracker/Migrations/20230130155934_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace eCO2TrackerWebAPI.Migrations
+namespace eCO2Tracker.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
