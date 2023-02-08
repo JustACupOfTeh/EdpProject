9004f621e729198eef32f202bf97a75f5a026957
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-08 18:43:16 +0800

    Add files via upload

---
+++ eCO2Tracker/Utility/CodeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace eCO2TrackerWebAPI.Utility
+{
+    public class CodeGenerator
+    {
+        // Instantiate random number generator.  
+        private readonly Random _random = new Random();
+
+        // Generates a random number within a range.      
+        public static string GetReferralCode()
+        {
+            const string src = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            int length = 10;
+            var sb = new StringBuilder();
+
+            Random RNG = new Random();
+            for (var i = 0; i < length; i++)
+            {
+                var c = src[RNG.Next(0, src.Length)];
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
