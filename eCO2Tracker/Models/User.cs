f143f88b2397c505fb851f1896bd55c985110a13
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-08 23:53:42 +0800

    Update User.cs

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -7,7 +7,9 @@ namespace eCO2Tracker.Models
         [Key]
         public string UserID { get; set; }
         public string UserName { get; set; } = string.Empty;
+        public string? Email { get; set; }
         public string Password { get; set; } = string.Empty;
+        public string? ReferralCode { get; set; }
         public int PointsCurrent { get; set; }
         public int PointsTotal { get; set; }
 
