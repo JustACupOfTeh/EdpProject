d6fc970f68558db9a38ff73dfab2b5b1fb82a0a9
ZoryAxen <88285648+ZoryAxen@users.noreply.github.com>
2023-02-08 23:51:47 +0800

    Update User.cs

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -1,32 +1,17 @@
-﻿using Microsoft.OpenApi.Any;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eCO2Tracker.Models
 {
     public class User
     {
-        public User() { }   
+        [Key]
+        public string UserID { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public int PointsCurrent { get; set; }
+        public int PointsTotal { get; set; }
 
-        [Required, Column("First_Name")]
-        public string? FirstName { get; set; }
-
-        [Required, Column("Last_Name")]
-        public string? LastName { get; set; }
-
-        [Required]
-        public string? Email { get; set; }
-
-        [Required, MinLength(12, ErrorMessage = "{0} length can't be less than {1}.")]
-        public string? Password { get; set; }
-
-        public string Role { get; set; }
-
-        [Column("Referral_Code")]
-        public string? ReferralCode { get; set; }
-
-        public DateTime CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-
+        //Relationships
+        public List<User_ShopItems>? User_ShopItems { get; set; }
     }
 }
