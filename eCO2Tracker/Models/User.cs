ac65d7124894c403f99de1b231a78eb20ed89f2f
Yi Shen <yishen890@gmail.com>
2023-02-12 23:36:44 +0800

    Updated user database. Added service to manipulate points

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -5,11 +5,24 @@ namespace eCO2Tracker.Models
     public class User
     {
         [Key]
-        public string UserID { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public int PointsCurrent { get; set; }
-        public int PointsTotal { get; set; }
+        public string? UserID { get; set; }
+        public string? UserName { get; set; }
+        [Required, Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Required, Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public int? PointsCurrent { get; set; }
+        public int? PointsTotal { get; set; }
+        public string? Role { get; set; }
+        [Display(Name = "Referral Code")]
+        public string? ReferralCode { get; set; }
+        [Display(Name = "Created Date")]
+        public DateTime? CreatedDate { get; set; }
+        [Display(Name = "Updated Date")]
+        public DateTime? UpdatedDate { get; set; }
 
         //Relationships
         public List<User_ShopItems>? User_ShopItems { get; set; }
