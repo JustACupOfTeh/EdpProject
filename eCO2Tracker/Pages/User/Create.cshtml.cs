bf92fdfb75549640d74b9bec8bb4402ba9603f0d
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-14 11:33:46 +0800

    Add files via upload

---
+++ eCO2Tracker/Pages/User/Create.cshtml.cs
@@ -0,0 +1,56 @@
+using App.Data;
+using App.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using MySqlConnector;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Pages.User
+{
+    public class CreateModel : PageModel
+    {
+       
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public string FirstName { get; set; }
+
+        [BindProperty]
+        public string LastName { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+       
+        public Task OnPost()
+        {
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(Password);
+            using (MySqlConnection connection = new MySqlConnection("Server=localhost; User=root; Pwd=; Port=3306; Database=userdb"))
+            {
+                string query = "INSERT INTO users (FirstName, LastName, Email, Password, CreatedDate, UpdatedDate) " +
+                               "VALUES (@FirstName, @LastName, @Email, @Password, @CreatedDate, @UpdatedDate);";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", FirstName);
+                    command.Parameters.AddWithValue("@LastName", LastName);
+                    command.Parameters.AddWithValue("@Email", Email);
+                    command.Parameters.AddWithValue("@Password",hashedPassword);
+                    command.Parameters.AddWithValue("@CreatedDate", DateTime.UtcNow);
+                    command.Parameters.AddWithValue("@UpdatedDate", DateTime.UtcNow);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
+
