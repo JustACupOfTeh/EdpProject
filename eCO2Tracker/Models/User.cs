bb491ba12fab3e261683d6142478bdc20ac03669
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-14 10:49:28 +0800

    Update User.cs

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -1,10 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eCO2Tracker.Models
 {
     public class User
     {
         [Key]
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        
+        [Required]
+        public string Email { get; set; }
+
+		[Required, MinLength(12, ErrorMessage = "{0} length can't be less than {1}.")]
+        
+        [Display(Name = "Created Date")]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Updated Date")]
+        public DateTime UpdatedDate { get; set; }
+		public string Password { get; set; }
+        public string LastName { get; set; }
         public string UserID { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
