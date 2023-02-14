d8b14ddcd84286a053ddf5e1b098a6e420be15ac
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-14 10:50:04 +0800

    Update User.cs

--- eCO2Tracker/Models/User.cs
+++ eCO2Tracker/Models/User.cs
@@ -16,18 +16,17 @@ namespace eCO2Tracker.Models
         [Required]
         public string Email { get; set; }
 
-		[Required, MinLength(12, ErrorMessage = "{0} length can't be less than {1}.")]
+	[Required, MinLength(12, ErrorMessage = "{0} length can't be less than {1}.")]
+	public string Password { get; set; }
         
         [Display(Name = "Created Date")]
         public DateTime CreatedDate { get; set; }
 
         [Display(Name = "Updated Date")]
         public DateTime UpdatedDate { get; set; }
-		public string Password { get; set; }
         public string LastName { get; set; }
         public string UserID { get; set; }
         public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public int PointsCurrent { get; set; }
         public int PointsTotal { get; set; }
 
