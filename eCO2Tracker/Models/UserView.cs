e029ea40a31a282843d5293a6d0675993e723206
ryantxng <96640264+ryantxng@users.noreply.github.com>
2023-02-14 11:38:26 +0800

    Add files via upload

---
+++ eCO2Tracker/Models/UserView.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Data
+{
+    public class UserView
+    {
+        public string? Id { get; set; }
+        [Required, Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+        [Required, Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Display(Name = "Created Date")]
+        public DateTime? CreatedDate { get; set; }
+        [Display(Name = "Updated Date")]
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
